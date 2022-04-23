c631680c4c22aed5afcd9d17f88b8473bc191936
quynhtl <tranlequynh.uet@gmail.com>
2022-04-23 21:39:17 +0700

    train.py

--- train.py
+++ train.py
@@ -7,7 +7,6 @@ from argparse import ArgumentParser
 # conda deactivate (trong TH đang ở môi trg khác dùng lệnh này để thoát khỏi enviroment đó r mới cài thư viện dưới nhé)
 # pip install tensorflow-addons
 import tensorflow_addons as tfa
-from optimizer_adamW import WeightDecayScheduler, lr_schedule, wd_schedule
 from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
 from tensorflow.keras.callbacks import Callback
 
@@ -18,16 +17,16 @@ if __name__ == "__main__":
     # Arguments users used when running command lines
     parser.add_argument('--model', default='resnet50', type=str,
                         help='Type of ConvNeXt model, valid option: convnext resnetxt')
-    parser.add_argument('--optimizer', default="AdamW", type=str,
-                        help='Type of optimizer, valid option: AdamW')
-    parser.add_argument('--lr', default=0.001,
-                        type=float, help='Learning rate')
-    parser.add_argument("--batch-size", default=128, type=int)
-    parser.add_argument("--epochs", default=100, type=int)
     parser.add_argument('--num-classes', default=10,
                         type=int, help='Number of classes')
-    parser.add_argument('--num-filters', default=16,
-    type=int, help='Number of filters')
+    parser.add_argument('--lr', default=0.001,
+                        type=float, help='Learning rate')
+    parser.add_argument('--weight-decay', default=1e-4,
+                        type=float, help='Weight decay')
+    parser.add_argument('--batch-size', default=32, type=int,
+                        help='Batch size')
+    parser.add_argument('--epochs', default=100, type=int,
+                        help='Number of training epoch')
     parser.add_argument('--image-size', default=224,
                         type=int, help='Size of input image')
     parser.add_argument('--image-channels', default=3,
@@ -36,9 +35,8 @@ if __name__ == "__main__":
                         help='Where training data is located')
     parser.add_argument('--valid-folder', default='', type=str,
                         help='Where validation data is located')
-    parser.add_argument('--class-mode', default='categorical', type=str, help='Class mode to compile')
-    parser.add_argument('--model-folder', default='output/',
-                        type=str, help='Folder to save trained model')  
+    parser.add_argument('--model-folder', default='.output/',
+                        type=str, help='Folder to save trained model')
 
     args = parser.parse_args()
 
@@ -52,20 +50,6 @@ if __name__ == "__main__":
         print('{}.{}: {}'.format(i, arg, vars(args)[arg]))
     print('===========================')
 
-    # Assign arguments to variables to avoid repetition 
-    optimizer = args.optimizer
-    train_folder = args.train_folder
-    valid_folder = args.valid_folder
-    batch_size =  args.batch_size
-    image_size = args.image_size
-    image_channels = args.image_channels
-    num_classes = args.num_classes
-    epoch = args.epochs
-    class_mode = args.class_mode
-    lr = args.lr
-    num_filters = args.num_filters
-
-    AUTO = tf.data.AUTOTUNE
     # Data loader
     if args.train_folder != '' and args.valid_folder != '':
         # Load train images from folder
@@ -77,13 +61,13 @@ if __name__ == "__main__":
     #Build model
     if args.model == 'resnet50':
         model = ResNet([3, 4, 6, 3], name='ResNet50')
-        model.build(input_shape=(None, 224, 224, 3))
+        model.build(input_shape=(None, args.image_size, args.image_size, args.image_channels))
     elif args.model == 'resnetxt':
         model = ResNext50()
-        model.build(input_shape=(None, 224, 224, 3))
+        model.build(input_shape=(None, args.image_size, args.image_size, args.image_channels))
     else:
         model = convnext(
-            input_shape=[image_size,image_size,image_channels],
+            input_shape=[args.image_size, args.image_size, args.image_channels],
             classes = args.num_classes,
         )
 
@@ -91,37 +75,21 @@ if __name__ == "__main__":
 
     # Traning
 
-    if optimizer == "AdamW":
-        optimizer =  tfa.optimizers.AdamW(learning_rate=lr_schedule(0), weight_decay=wd_schedule(0))
-        
-        lr_callback = tf.keras.callbacks.LearningRateScheduler(lr_schedule)
-        
-        wd_callback = WeightDecayScheduler(wd_schedule)
-        model.compile(optimizer=optimizer, loss='categorical_crossentropy',
-                                         metrics=['accuracy'])
-        model.fit(train_ds, 
-                    validation_data=val_ds, 
-                    epochs=epoch,
-                    callbacks=[lr_callback, wd_callback],
-                    verbose=1)
-        model.save(args.model_folder)
-
-    else: 
-        optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
-        learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', 
-                                            patience=5, 
-                                            verbose=1, 
-                                            factor=0.5, 
-                                            min_lr=0.00001)
-        checkpoint = ModelCheckpoint(filepath=args.model_folder + 'model.h5', monitor='val_accuracy', mode='max', save_best_only=True, save_weights_only=False, verbose=1)
-        callbacks = [learning_rate_reduction, checkpoint] 
-        model.compile(optimizer=optimizer, loss='categorical_crossentropy',
-                                         metrics=['accuracy'])
-        model.fit(train_ds,
-                    epochs=epoch,
-                    callbacks=callbacks,
-                    validation_data=val_ds,
-                    verbose=1)
+    optimizer = tfa.optimizers.AdamW(learning_rate=args.lr, weight_decay=args.weight_decay)
+    learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', 
+                                        patience=5, 
+                                        verbose=1, 
+                                        factor=0.5, 
+                                        min_lr=0.00001)
+    checkpoint = ModelCheckpoint(filepath=args.model_folder + 'model.h5', monitor='val_accuracy', mode='max', save_best_only=True, save_weights_only=False, verbose=1)
+    callbacks = [learning_rate_reduction, checkpoint] 
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy',
+                                        metrics=['accuracy'])
+    model.fit(train_ds,
+                epochs=args.epoch,
+                callbacks=callbacks,
+                validation_data=val_ds,
+                verbose=1)
 
     
     
